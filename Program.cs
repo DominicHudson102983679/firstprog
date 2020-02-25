4e9c89929c9db0a550acaba8c95edd5f666d913c
DOMINIC HUDSON <102983679@student.swin.edu.au>
2020-02-25 16:42:59 +1100

    firstprog

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace firstprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your firstname");
+            string firstName = Console.ReadLine{};
+
+            Console.WriteLine("Whats your surname");
+            string firstName = Console.ReadLgit iine{};
+
+            Console.WriteLine{firstname};
+            Console.WriteLine{surname};
+        }
+    }
+}
